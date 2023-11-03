3070b0a80340432f39634c4740c8d655411bb071
S-Hinkle <shcatchall@protonmail.com>
2023-11-03 10:01:55 -0400

    Added routes

--- 3-express/express.js
+++ 3-express/express.js
@@ -14,7 +14,39 @@ const getPetsObj = async () => {
   return JSON.parse(petData);
 };
 
-
+// Define a route to handle GET requests for the list of all pets
+app.get('/pets', async (req, res) => {
+    try {
+      const pets = await getPetsObj();
+      res.status(200).json(pets);
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+  // Define a route to handle GET requests for a single pet by index
+  app.get('/pets/:id', async (req, res) => {
+    try {
+      const index = parseInt(req.params.id, 10);
+      if (isNaN(index)) {
+        return res.status(400).send('Bad Request: Invalid pet index');
+      }
+  
+      const pets = await getPetsObj();
+      if (index >= pets.length || index < 0) {
+        res.status(404).send('Not Found');
+      } else {
+        res.status(200).json(pets[index]);
+      }
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+  // Handle 404 for any other routes
+  app.use((req, res) => {
+    res.status(404).send('Not Found');
+  });
 
 // Define the port number on which the server will listen for requests
 const PORT = 3000;
