8c5e3cb182495e89559db70fefcb6cdca55956c8
S-Hinkle <shcatchall@protonmail.com>
2023-11-09 13:26:19 -0500

    Added migration.sql, seed.sql, and .env file

---
+++ 4-restful/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO pets (age, kind, name) VALUES (5, 'Dog', 'Rex');
+INSERT INTO pets (age, kind, name) VALUES (3, 'Cat', 'Whiskers');
+INSERT INTO pets (age, kind, name) VALUES (2, 'Parrot', 'Polly');
